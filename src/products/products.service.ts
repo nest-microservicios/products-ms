d34c40fb730e6c30191645505449d2777d111df5
Pablo Alejandro Sangenis <pasangenis@teco.com.ar>
2024-04-26 22:00:20 -0300

    Cmabios en los servicios y agrego sql para generar datos

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,14 +1,10 @@
 import { PrismaClient } from '@prisma/client';
-import {
-  Injectable,
-  Logger,
-  NotFoundException,
-  OnModuleInit,
-} from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 
 import { PaginationDto } from 'src/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -47,7 +43,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       where: { id, available: true },
     });
     if (!product) {
-      throw new NotFoundException(`Product with id #${id} not found`);
+      throw new RpcException({
+        status: HttpStatus.BAD_REQUEST,
+        message: `Product with id #${id} not found`,
+      });
     }
     return product;
   }
